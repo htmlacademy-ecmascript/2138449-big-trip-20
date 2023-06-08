2ba3fa851a1dbbd8886b693d024f52b506abe55f
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-06-08 21:44:16 +0300

    презентер для новой точки

---
+++ src/presenter/new-point-presenter.js
@@ -0,0 +1,78 @@
+import { remove, render, RenderPosition } from '../framework/render.js';
+import PointEditView from '../view/edit-point-view.js';
+import {nanoid} from 'nanoid';
+
+import { UserAction, UpdateType } from '../const.js';
+
+export default class NewPointPresenter {
+  #pointListContainer = null;
+
+  #handleDataChange = null;
+  #handleDestroy = null;
+
+  #destinationsModel = null;
+  #offersModel = null;
+
+  #pointEditComponent = null;
+
+  constructor({pointListContainer, onDataChange, onDestroy, destinationsModel, offersModel}) {
+    this.#pointListContainer = pointListContainer;
+    this.#handleDataChange = onDataChange;
+    this.#handleDestroy = onDestroy;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
+  }
+
+  init() {
+    if (this.#pointEditComponent !== null) {
+      return;
+    }
+
+    this.#pointEditComponent = new PointEditView({
+      destinationModel: this.#destinationsModel,
+      offersModel: this.#offersModel,
+      onFormSubmit: this.#handleFormSubmit,
+      onDeleteClick: this.#handleDeleteClick,
+      isNew: true,
+    });
+
+    render(this.#pointEditComponent, this.#pointListContainer, RenderPosition.AFTERBEGIN);
+
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  }
+
+  destroy() {
+    if (this.#pointEditComponent === null) {
+      return;
+    }
+
+    this.#handleDestroy();
+
+    remove(this.#pointEditComponent);
+    this.#pointEditComponent = null;
+
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  }
+
+  #handleFormSubmit = (point) => {
+    this.#handleDataChange(
+      UserAction.ADD_POINT,
+      UpdateType.MINOR,
+      // Пока у нас нет сервера, который бы после сохранения
+      // выдывал честный id задачи, нам нужно позаботиться об этом самим
+      {id: nanoid(), ...point},
+    );
+    this.destroy();
+  };
+
+  #handleDeleteClick = () => {
+    this.destroy();
+  };
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.destroy();
+    }
+  };
+}
