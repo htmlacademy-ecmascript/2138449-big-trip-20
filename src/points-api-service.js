af0489cc40ea27b9e2345225be82cdc481779ec1
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-06-14 17:28:44 +0300

    реализация паттерна Адаптер

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -27,7 +27,7 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${update.id}`,
       method: Method.PUT,
-      body: JSON.stringify(update),
+      body: JSON.stringify(this.#adaptToServer(update)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
@@ -40,7 +40,7 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: 'points',
       method: Method.POST,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
@@ -58,5 +58,26 @@ export default class PointsApiService extends ApiService {
     return response;
   }
 
-  
+  #adaptToServer(point) {
+    const adaptedPoint = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'is_favorite': point.isFavorite,
+    };
+
+    adaptedPoint.destination = adaptedPoint.destination.id;
+
+    if (adaptedPoint.offers.length) {
+      adaptedPoint.offers = adaptedPoint.offers.map((elem) => elem.id);
+    }
+
+    delete adaptedPoint.isFavorite;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.waypoint;
+
+    return adaptedPoint;
+  }
 }
