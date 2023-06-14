a57346ebe9c9c5d1a80192ffdbcdcc8e11f08ffe
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-06-14 20:53:02 +0300

    точки не обновляются

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -38,20 +38,26 @@ export default class PointsModel extends Observable {
     this._notify(UpdateType.INIT);
   }
 
-  updatePoint(updateType, update) {
+  async updatePoint(updateType, update) {
     const index = this.points.findIndex((point) => point.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
+    try {
+      const response = await this.#service.updatePoint(update);
+      const updatePoint = this.#adaptToClient(response);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatePoint,
+        ...this.#points.slice(index + 1),
+      ];
 
-    this.#points = [
-      ...this.#points.slice(0, index),
-      update,
-      ...this.#points.slice(index + 1),
-    ];
+      this._notify(updateType, updatePoint);
+    } catch(err) {
+      throw new Error('Can\'t update point');
+    }
 
-    this._notify(updateType, update);
   }
 
   addPoint(updateType, update) {
